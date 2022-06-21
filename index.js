d4e42a82afbaeca5e5fbcde4f734257bc38babb8
QuackOri <103168837+QuackOri@users.noreply.github.com>
2022-06-22 05:00:26 +0900

    BodyParser 설치, PostMan 설치, 회원 가입 기능 만듦. BodyParser: client에서 보내준 자료들을 받을 수 있게 해줌. PostMan: client request 요청을 만들어줌.

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
 
-const mongoose = require('mongoose')
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://min:als622@boilerplate.i8mg4.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 DB에 넣어준다.
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => { // 이건 몽고DB 메서드, DB에 저장한다는 의미
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
