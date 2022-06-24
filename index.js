26d4fb59e3a60861fd0c161bb0d1111b8fe80294
QuackOri <103168837+QuackOri@users.noreply.github.com>
2022-06-25 04:41:35 +0900

    auth 기능 추가. token을 decode해서 user._id를 얻어 DB에서 token과 _id로 유저를 찾는다. 있다면 성공. auth를 미들웨어로 만들어서 인증에 성공했다면 req에 user 정보와 token을 저장해 다음단계로 넘어간다.

--- index.js
+++ index.js
@@ -1,83 +1,96 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const config = require('./config/key');
-
-const {User} = require("./models/User");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+const { auth } = require("./middleware/auth");
+const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Hello Backend!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! Hello Backend!");
+});
 
-app.post('/register', (req, res) => {
-    
-    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
-    //그것들을 DB에 넣어준다.
+app.post("/api/users/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 DB에 넣어준다.
 
-    const user = new User(req.body);
+  const user = new User(req.body);
 
-    user.save((err, userInfo) => { // 이건 몽고DB 메서드, DB에 저장한다는 의미
-        if(err) return res.json({success: false, err})
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
+  user.save((err, userInfo) => {
+    // 이건 몽고DB 메서드, DB에 저장한다는 의미
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
-app.post('/login', (req, res) => {
-
+// login
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 DB에서 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
-    if(!user){
+    if (!user) {
       return res.json({
         loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다."
-      })
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
     }
 
     // 요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인한다.
     user.comparePassword(req.body.password, (err, isMatch) => {
-      if(!isMatch)
-      return res.json({
-        loginSuccess: false, message: "비밀번호가 틀렸습니다."
-      })
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
 
       // 비밀번호까지 맞다면 토큰을 생성하기.
       user.generateToken((err, user) => {
-        if(err) returnres.status(400).send(err);
+        if (err) returnres.status(400).send(err);
 
         // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
-        res.cookie("x_auth", user.token)
-        .status(200)
-        .json({ loginSuccess:true, userId: user._id })
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
 
-      })
-
-    })
-
-  })
-
-
-
-  
-
-})
+//auth, 아래 파라미터중 중간 auth는 미들웨어.
+// role 0 -> 일반유저   role !0 -> 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이  True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
