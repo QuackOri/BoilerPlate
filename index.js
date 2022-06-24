248136966189f956df74366c44f92e8416bacf6b
QuackOri <103168837+QuackOri@users.noreply.github.com>
2022-06-25 04:53:47 +0900

    로그아웃 기능 추가. /api/users/logout get 요청이 들어오면 해당 user의 _id로 찾아 token을 지운다.

--- index.js
+++ index.js
@@ -91,6 +91,14 @@ app.get("/api/users/auth", auth, (req, res) => {
   });
 });
 
+// logout
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
